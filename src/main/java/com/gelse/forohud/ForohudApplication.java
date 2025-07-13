cf484737ad4db01b47f92de6ea62cfeeaa85a1b5
Jose-Angel-12 <jajaraba12@gmail.com>
2025-07-12 19:06:55 -0500

    cargando proyecto

---
+++ src/main/java/com/gelse/forohud/ForohudApplication.java
@@ -0,0 +1,13 @@
+package com.gelse.forohud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohudApplication.class, args);
+	}
+
+}

dc0392283f883b4c71c674157e3021337a6538eb
Jose-Angel-12 <jajaraba12@gmail.com>
2025-07-14 10:56:33 -0500

    insertando la tabla topicos a la base de datos

--- src/main/java/com/gelse/forohud/controller/TopicoController.java
+++ src/main/java/com/gelse/forohud/controller/TopicoController.java
@@ -1,6 +1,10 @@
 package com.gelse.forohud.controller;
 
 import com.gelse.forohud.topico.DatosRegistroTopico;
+import com.gelse.forohud.topico.Topico;
+import com.gelse.forohud.topico.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/topico")
 public class TopicoController {
+    @Autowired
+    private TopicoRepository repository;
 
+    @Transactional
     @PostMapping
-    public void registroTopico(@RequestBody DatosRegistroTopico datos){
-        System.out.println(datos);
+    public void registroTopico(@RequestBody DatosRegistroTopico datos) {
+        repository.save(new Topico(datos));
     }
 
 }
---
+++ src/main/java/com/gelse/forohud/topico/Topico.java
@@ -0,0 +1,33 @@
+package com.gelse.forohud.topico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private String autor;
+    private String curso;
+
+    public Topico(DatosRegistroTopico datos) {
+        this.id = null;
+        this.titulo = datos.titulo();
+        this.mensaje = datos.mensaje();
+        this.autor = datos.autor();
+        this.curso = datos.curso();
+    }
+}

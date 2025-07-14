6272eaee3eb6bc1d283fa3df5e1cb95278c5c8fa
Jose-Angel-12 <jajaraba12@gmail.com>
2025-07-14 12:14:52 -0500

    incluyendo validaciones

--- src/main/java/com/gelse/forohud/controller/TopicoController.java
+++ src/main/java/com/gelse/forohud/controller/TopicoController.java
@@ -3,6 +3,7 @@ package com.gelse.forohud.controller;
 import com.gelse.forohud.topico.DatosRegistroTopico;
 import com.gelse.forohud.topico.Topico;
 import com.gelse.forohud.topico.TopicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ public class TopicoController {
 
     @Transactional
     @PostMapping
-    public void registroTopico(@RequestBody DatosRegistroTopico datos) {
+    public void registroTopico(@RequestBody @Valid DatosRegistroTopico datos) {
         repository.save(new Topico(datos));
     }
 
--- src/main/java/com/gelse/forohud/topico/DatosRegistroTopico.java
+++ src/main/java/com/gelse/forohud/topico/DatosRegistroTopico.java
@@ -1,9 +1,19 @@
 package com.gelse.forohud.topico;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record DatosRegistroTopico(
+        @NotBlank(message = "El Titulo no puede estar en blanco")
+        @Pattern(regexp = "^.{1,100}$", message = "El Titulo solo puede tener de 1 a 100 caracteres")
         String titulo,
+        @NotBlank(message = "El Titulo no puede estar en blanco")
+        @Pattern(regexp = "^.{1,800}$", message = "El Mensaje solo puede tener de 1 a 800 caracteres")
         String mensaje,
+        @NotBlank(message = "El Autor no puede estar en blanco")
         String autor,
+        @NotBlank(message = "El Curso no puede estar en blanco")
         String curso
-        ) {
+) {
 }

f627f8859c42f9fc870d955b04193dca73001959
Jose-Angel-12 <jajaraba12@gmail.com>
2025-07-14 17:07:11 -0500

    update

--- src/main/resources/db/migration/V1__create-table-topicos.sql
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -1,9 +1,9 @@
 create table topicos(
 
     id bigint not null auto_increment,
-    titulo  varchar(100) not null,
-    mensaje varchar(100) not null,
-    autor varchar(100) not null unique,
+    titulo  varchar(100) not null unique,
+    mensaje varchar(100) not null unique,
+    autor varchar(100) not null,
     curso varchar(100) not null,
 
     primary key(id)
